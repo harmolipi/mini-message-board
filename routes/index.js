a5a92b496ea2209b86a794863cd4bd236bb592a1
harmolipi <nikob381@gmail.com>
2022-02-13 02:11:22 -0500

    Added new message routes

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,20 @@ const messages = [
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express', messages: messages });
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+router.get('/new', (req, res) => {
+  res.render('form');
+});
+
+router.post('/new', (req, res) => {
+  messages.push({
+    text: req.body.messageText,
+    user: req.body.userName,
+    added: new Date(),
+  });
+  res.redirect('/');
 });
 
 module.exports = router;
